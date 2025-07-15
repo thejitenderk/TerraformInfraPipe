31a406f58f264c72f2ec22611bf9867f708ceb15
Jitender Kumar <thejitenderk@gmail.com>
2025-07-15 01:34:24 -0400

    jitejdjekrjerk

--- Infra/variable.tf
+++ Infra/variable.tf
@@ -1,9 +1,9 @@
 variable "rg_name" {
- 
+ description = "values for resource group names and locations"
 }
 
 variable "vnet_name" {
-
+  description = "values for virtual network names, resource group names, and address spaces"
 }
 
 variable "subnet_name" {}
